0ebb6dc85d6290c23e346fc1ad69e60fdcdf93f9
unknown <KM70576@dir.kmt.kmtg.net>
2021-09-17 13:02:22 +0900

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://12d2480354e94b2f8a614aea7ef9727b@o1003254.ingest.sentry.io/5963670",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
